d649d78bf07ab399b3c5dcfb49f843dadae9cad9
Jack Neil <25892470+jackneil@users.noreply.github.com>
2019-07-14 13:23:42 -0400

    fixed utf-8 encoding on read

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -29,7 +29,7 @@ def load_dataset(enc, path, combine):
                     token_chunks.append(npz[item])
         else:
             # Plain text
-            with open(path, 'r') as fp:
+            with open(path, 'r', encoding='utf-8') as fp:
                 raw_text += fp.read()
             print("\nRead {:,} chars from {} ...".format(len(raw_text), path))
             if len(raw_text) >= combine:
